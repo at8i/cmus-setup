cf5491412077c54c3ec16ec7e435f92fdbde1060
at8i <amir.tusi@gmail.com>
2021-01-07 23:15:31 +0330

    Updated cmus-plugins.sh

--- cmus-plugins.sh
+++ cmus-plugins.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 cmus-feh.sh "$*" &
 cmus-artget.sh "$*" &
-if pgrep -x terminator > /dev/null
-then
-    :
-else
-    terminator -x cmus-lyrics "$*" &
+
+CM_ST=$(cmus-remote -Q 2>/dev/null | grep "playing")
+
+if [[ -n "$CM_ST" ]]; then
+    if pgrep -x terminator > /dev/null
+         then
+             :
+         else
+             terminator -x cmus-lyrics "$*"
+    fi
 fi
